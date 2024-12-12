0c723a9ad2bbf57320f6b840b4b9706988b26d0c
Erwin <erwin.paralesda@gmail.com>
2024-12-12 01:17:04 -0500

    Add files via upload

---
+++ Demo.ArquitectureNetAdvanced/Demo.Arquitecture.Transversal.Mapper/MappingProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Demo.Arquitecture.Aplication.DTO;
+using Demo.Arquitecture.Domain.Entity;
+
+namespace Demo.Arquitecture.Transversal.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Client, ClientDTO>().ReverseMap();
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Shooping, ShoopingDTO>().ReverseMap();
+            CreateMap<PurchaseDetail, PurchaseDetailDTO>().ReverseMap();
+            CreateMap<Purchase, PurchaseDTO>().ReverseMap();
+
+            CreateMap<Pet, PurchaseDTO>().ReverseMap();
+        }
+    }
+}

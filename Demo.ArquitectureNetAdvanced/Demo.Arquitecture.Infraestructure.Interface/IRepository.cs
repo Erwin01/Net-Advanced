42f7a61eaaf80084c3fe631fe02df365f20078d5
Erwin <erwin.paralesda@gmail.com>
2024-12-12 01:15:18 -0500

    Add files via upload

---
+++ Demo.ArquitectureNetAdvanced/Demo.Arquitecture.Infraestructure.Interface/IRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Demo.Arquitecture.Infraestructure.Interface
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T> Add(T obj);
+        Task<T> Update(T obj, int id);
+        Task<bool> Delete(int id);
+        Task<List<T>> GetAll();
+        Task<T> GetById(int id);
+        Task<List<T>> GetByParam(Func<T, bool> func);
+        Task<List<T>> AddList(List<T> obj);
+        void Save(T obj, int id);
+    }
+}
